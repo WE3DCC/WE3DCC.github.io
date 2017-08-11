16cc21ac6f61a45755935021f2b71cd242c0bcae
WE3DCC <WE3DCC@imac.attlocal.net>
2017-08-11 16:03:19 -0700

    register aframe href // update clickcomp

--- scripts/clickcomp.js
+++ scripts/clickcomp.js
@@ -4,6 +4,8 @@ AFRAME.registerComponent('cliccomp', {
     this.el.addEventListener('click', function (evt) {
       var randomIndex = Math.floor(Math.random() * COLORS.length);
       this.setAttribute('material', 'color', COLORS[randomIndex]);
+      require('aframe');
+      require('aframe-href-component');
       console.log('I was clicked at: ', evt.detail.intersection.point);
     });
   }
